be9e93b0ea8807c9599e02c0402471a8a13a6971
Jia Lihong <im-red@qq.com>
2020-06-27 15:10:14 +0800

    Update test_object.cpp

--- test/test_object.cpp
+++ test/test_object.cpp
@@ -14,58 +14,58 @@ using namespace std;
 TEST(object, CONSTRUCT)
 {
     {
-        Object *o = new Object(0);
-        EXPECT_EQ(o->category(), Object::Category::Int);
-        EXPECT_EQ(o->intData(), 0);
+        Object o(0);
+        EXPECT_EQ(o.category(), Object::Category::Int);
+        EXPECT_EQ(o.intData(), 0);
     }
     {
-        Object *o = new Object(0.0f);
-        EXPECT_EQ(o->category(), Object::Category::Float);
-        EXPECT_EQ(o->floatData(), 0.0);
+        Object o(0.0f);
+        EXPECT_EQ(o.category(), Object::Category::Float);
+        EXPECT_EQ(o.floatData(), 0.0);
     }
     {
-        Object *o = new Object("test");
-        EXPECT_EQ(o->category(), Object::Category::String);
-        EXPECT_EQ(o->stringData(), "test");
+        Object o("test");
+        EXPECT_EQ(o.category(), Object::Category::String);
+        EXPECT_EQ(o.stringData(), "test");
     }
     {
-        Object *o = new Object(Object::Category::Struct, 5);
-        EXPECT_EQ(o->category(), Object::Category::Struct);
-        EXPECT_EQ(o->elementCount(), 5);
+        Object o(Object::Category::Struct, 5);
+        EXPECT_EQ(o.category(), Object::Category::Struct);
+        EXPECT_EQ(o.elementCount(), 5);
     }
     {
-        Object *o = new Object(Object::Category::List, 5);
-        EXPECT_EQ(o->category(), Object::Category::List);
-        EXPECT_EQ(o->elementCount(), 5);
+        Object o(Object::Category::List, 5);
+        EXPECT_EQ(o.category(), Object::Category::List);
+        EXPECT_EQ(o.elementCount(), 5);
     }
 }
 
 TEST(object, STRUCT)
 {
-    Object *o = new Object(Object::Category::Struct, 1);
-    o->field(0).setCategory(Object::Category::Int);
-    o->field(0).setIntData(0);
-    EXPECT_EQ(o->field(0).category(), Object::Category::Int);
-    EXPECT_EQ(o->field(0).intData(), 0);
+    Object o(Object::Category::Struct, 1);
+    o.field(0).setCategory(Object::Category::Int);
+    o.field(0).setIntData(0);
+    EXPECT_EQ(o.field(0).category(), Object::Category::Int);
+    EXPECT_EQ(o.field(0).intData(), 0);
 }
 
 TEST(object, LIST)
 {
-    Object *o = new Object(Object::Category::List, 0);
-    o->append(Object(0));
-    EXPECT_EQ(o->elementCount(), 1);
-    EXPECT_EQ(o->at(0).category(), Object::Category::Int);
-    EXPECT_EQ(o->at(0).intData(), 0);
+    Object o(Object::Category::List, 0);
+    o.append(Object(0));
+    EXPECT_EQ(o.elementCount(), 1);
+    EXPECT_EQ(o.at(0).category(), Object::Category::Int);
+    EXPECT_EQ(o.at(0).intData(), 0);
 }
 
 TEST(object, ASSIGN)
 {
-    Object *o = new Object(Object::Category::List, 0);
-    o->append(Object(0));
-    EXPECT_EQ(o->elementCount(), 1);
-    EXPECT_EQ(o->at(0).category(), Object::Category::Int);
-    EXPECT_EQ(o->at(0).intData(), 0);
+    Object o(Object::Category::List, 0);
+    o.append(Object(0));
+    EXPECT_EQ(o.elementCount(), 1);
+    EXPECT_EQ(o.at(0).category(), Object::Category::Int);
+    EXPECT_EQ(o.at(0).intData(), 0);
 
-    Object *copy = new Object(*o);
-    EXPECT_TRUE(*o == *copy);
+    Object copy = o;
+    EXPECT_TRUE(o == copy);
 }
